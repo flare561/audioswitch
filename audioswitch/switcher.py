53c88853df381de46802e3730141bf09b79a1adb
flare561 <flare561@gmail.com>
2017-04-19 02:54:34 -0500

    refactored switcher a bit

--- audioswitch/switcher.py
+++ audioswitch/switcher.py
@@ -4,10 +4,12 @@ from os import system
 
 HEADPHONES = "'Stereo Headphones FP'"
 SPEAKERS = "'Stereo Headphones'"
-
+OXYGEN_FILE = "/proc/asound/DG/oxygen"
+JACK_STATUS_LOCATION = 551
+OUTPUT_DEVICE = "'Analog Output'"
     
 def set_active(output):
-    system(f"amixer set 'Analog Output' {output}")
+    system(f"amixer set {OUTPUT_DEVICE} {output}")
 
 def headphones_state(oxygen_file):
     # Python seems to cache the file if we seek directly to
@@ -17,7 +19,7 @@ def headphones_state(oxygen_file):
     # The oxygen file contains a list of hex codes representing the
     # state of the sound card. The single hex digit located at position
     # 551 contains the state of the headphone jack.
-    oxygen_file.seek(551)
+    oxygen_file.seek(JACK_STATUS_LOCATION)
     byte = int(oxygen_file.read(1), 16)
 
     # The first bit (from left to right) represents whether the headphone
@@ -27,7 +29,7 @@ def headphones_state(oxygen_file):
     return (not (byte & 0b1), not (byte >> 3 & 0b1))
 
 def main():
-    with open("/proc/asound/DG/oxygen", "rb") as f:
+    with open(OXYGEN_FILE, "rb") as f:
         while True:
             plugged_in, active = headphones_state(f)
             if plugged_in and not active:
